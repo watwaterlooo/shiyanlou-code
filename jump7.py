ef909f7a3ca7f10b7f1af8e9b3e92c7cdae5c84e
watwaterlooo <hlcater@126.com>
2020-10-10 18:45:33 +0800

    jump7 with for or while in shiyanlou PY course from scratch

---
+++ jump7.py
@@ -0,0 +1,17 @@
+# use while 
+# a=1
+# while a<101:
+# 	if a%7==0 or a//10==7 or a%10==7:
+# 		continue
+# 	else:
+# 		print(a)
+# 	a+=1
+
+# use for
+for a in range(1,101):
+	if a%7==0 or a//10==7 or a%10==7:
+		continue
+	else:
+		print(a)
+
+#
